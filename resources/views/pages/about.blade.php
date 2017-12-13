e1a68c0c5255a0fea21712c37c5d03fabef813e9
miguel02r <miguel02r@hotmail.com>
2017-12-12 21:07:20 -0500

    Creating repo

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+   <h1>{{$title}}</h1>
+    <p>Parrafo de prueba</p>
+@endsection
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="jumbotron text-center">
+        <h1>{{$title}}</h1>
+        <p>Yo yo</p>
+    </div>
+@endsection
+
